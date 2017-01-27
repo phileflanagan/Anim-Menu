42793959d8aee06ee28be52ec71832deff5befb5
Phil Flanagan <phileflanagan@gmail.com>
2017-01-27 00:39:15 -0500

    Begin to work out JS for animation transition

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
-console.log("test");
+const spinningMenu = document.querySelector('.spinningmenu');
+const spinningContainer = spinningMenu.querySelector('ul');
+const spinningElements = spinningContainer.querySelectorAll('li');
+const lastSpinningElement = spinningContainer.querySelector('li:last-child');
+
+
+const rotationDuration = 15; //seconds
+// lastSpinningElement.style.borderColor = 'yellow';
+function endInitSpinning() {
+  spinningElements.forEach((el, i) => {
+    el.style.animation = `rot ${rotationDuration}s infinite 0.0s linear`;
+  });
+}
+
+lastSpinningElement.addEventListener('animationiteration', endInitSpinning)
