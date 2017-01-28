761ee8b2b842f90310afcc75b985df5b1d5cec1b
Phil Flanagan <phileflanagan@gmail.com>
2017-01-28 01:20:19 -0500

    Add second animation

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,15 +1,21 @@
 const spinningMenu = document.querySelector('.spinningmenu');
 const spinningContainer = spinningMenu.querySelector('ul');
 const spinningElements = spinningContainer.querySelectorAll('li');
-const lastSpinningElement = spinningContainer.querySelector('li:last-child');
+const lastSpinningElement = spinningContainer.querySelector('li:first-child');
 
 
-const rotationDuration = 15; //seconds
-// lastSpinningElement.style.borderColor = 'yellow';
-function endInitSpinning() {
-  spinningElements.forEach((el, i) => {
-    el.style.animation = `rot ${rotationDuration}s infinite 0.0s linear`;
+// rotation duration complete (in seconds)
+const rotationDuration = 15;
+// calculated duration of single item
+const rotationDurationSingle = rotationDuration / spinningElements.length;
+// set initial position of elements
+
+function initializeSpin() {
+  spinningElements.forEach((el,i) => {
+    // el.style.transform = `rotate(${360/spinningElements.length * i}deg) translate(-250px) rotate(-${360/spinningElements.length * i}deg)`;
+    el.style.animation = `rot ${rotationDuration}s infinite -${rotationDurationSingle * i}s linear`;
+    spinningContainer.classList.add = 'is-spinning';
   });
 }
 
-lastSpinningElement.addEventListener('animationiteration', endInitSpinning)
+initializeSpin();
