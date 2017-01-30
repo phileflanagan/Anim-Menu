cd33048f6c1192178b64dfad14ca5f1036abd02f
Phil Flanagan <phileflanagan@gmail.com>
2017-01-30 01:44:41 -0500

    Refactor scss

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,16 +5,14 @@ const lastSpinningElement = spinningContainer.querySelector('li:first-child');
 
 
 // rotation duration complete (in seconds)
-const rotationDuration = 15;
+const rotationDuration = 30;
 // calculated duration of single item
 const rotationDurationSingle = rotationDuration / spinningElements.length;
 // set initial position of elements
 
 function initializeSpin() {
   spinningElements.forEach((el,i) => {
-    // el.style.transform = `rotate(${360/spinningElements.length * i}deg) translate(-250px) rotate(-${360/spinningElements.length * i}deg)`;
     el.style.animation = `rot ${rotationDuration}s infinite -${rotationDurationSingle * i}s linear`;
-    spinningContainer.classList.add = 'is-spinning';
   });
 }
 
